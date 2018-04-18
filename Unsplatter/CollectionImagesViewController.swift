3404570b8bbc1face35b30eb9b581e58c9d8ee06
Anastasia Kapinos <anastasia.kapinos@gmail.com>
2018-04-18 14:38:29 +0300

    initial commit

---
+++ Unsplatter/CollectionImagesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionImagesViewController.swift
+//  Unsplatter
+//
+//  Created by Anastasia on 4/18/18.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class CollectionImagesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Unsplatter/DetailsImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsImageViewController.swift
+//  Unsplatter
+//
+//  Created by Anastasia on 4/18/18.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class DetailsImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
